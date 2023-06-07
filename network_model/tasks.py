ed65d865eddbad9f43ca5fed6989172eddb1a2f3
fluxx1on <nick.kaliga@ya.ru>
2023-06-07 15:56:01 +0300

    no venv

--- network_model/tasks.py
+++ network_model/tasks.py
@@ -5,11 +5,4 @@ from server.settings import DEFAULT_FROM_EMAIL
 
 @shared_task
 def send_email():
-    email = EmailMessage(
-        'Test email from SendGrid',
-        'This is a test email from SendGrid',
-        DEFAULT_FROM_EMAIL,
-        ['nick.kaliga@ya.ru'],
-    )
-    email.send(fail_silently=False)
-    send_email.apply_async(countdown=60)
+    
